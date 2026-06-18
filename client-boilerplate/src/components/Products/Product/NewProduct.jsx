ec65ecdd3c49b45fa8ba065c55c112fa95fdecf5
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-18 15:55:16 +0530

    product

---
+++ client-boilerplate/src/components/Products/Product/NewProduct.jsx
@@ -0,0 +1,12 @@
+
+
+const NewProduct = () => {
+  return (
+    <div>
+      <h2>Create New Product</h2>
+      {/* Add your form fields here */}
+    </div>
+  );
+};
+
+export default NewProduct;
